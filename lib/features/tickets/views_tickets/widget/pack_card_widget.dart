b9135b471e4244f06ef9622a0a855b438eef9112
unknown <sakriwassim@gmail.com>
2022-10-23 02:30:36 +0100

    get ui packs 15

--- lib/features/tickets/views_tickets/widget/pack_card_widget.dart
+++ lib/features/tickets/views_tickets/widget/pack_card_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PackCardWidget extends StatelessWidget {
   String libelle;
@@ -10,118 +11,59 @@ class PackCardWidget extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: InkWell(
         onTap: () {},
-        child: SizedBox(
-          height: 300,
-          width: 244,
-          child: Stack(
-            children: [
-              Positioned(
-                bottom: 0,
-                child: Container(
-                  height: 265,
-                  width: 244,
-                  color: Colors.white,
-                ),
+        child: Container(
+          width: 300,
+          child: Stack(children: <Widget>[
+            Image.asset("assets/packs_bg/$libelle.png"),
+            //Image.asset("assets/packs_bg/gold.png"),
+            Container(
+              height: 250,
+              width: 300,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    libelle,
+                    style: TextStyle(
+                        fontFamily: 'AirbnbCereal',
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    '10 euro',
+                    style: TextStyle(
+                        fontFamily: 'AirbnbCereal',
+                        fontSize: 30,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    '3 évenèments',
+                    style: const TextStyle(
+                        fontFamily: 'AirbnbCereal',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    '7 jours de publicité',
+                    style: const TextStyle(
+                        fontFamily: 'AirbnbCereal',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    textAlign: TextAlign.center,
+                    'Acheter',
+                    style: TextStyle(
+                        fontFamily: 'AirbnbCereal',
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ],
               ),
-              Positioned(
-                  top: 0,
-                  left: 20,
-                  child: Stack(
-                    children: [
-                      Image.asset('assets/Rectangle 4291.png'),
-                      Positioned(
-                          top: 21,
-                          left: 50.5,
-                          child: Text(
-                            libelle,
-                            style: TextStyle(
-                                fontFamily: 'AirbnbCereal',
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500),
-                          ))
-                    ],
-                  )),
-              const Positioned(
-                left: 70,
-                top: 75,
-                child: Text(
-                  '10 euro',
-                  style: TextStyle(
-                      fontFamily: 'AirbnbCereal',
-                      fontSize: 30,
-                      fontWeight: FontWeight.w300),
-                ),
-              ),
-              Positioned(bottom: 0, child: Image.asset('assets/Vector 19.png')),
-              Positioned(
-                left: 60,
-                top: 175,
-                child: Row(
-                  children: [
-                    Image.asset('assets/darkTick.png'),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      '3 évenèments',
-                      style: const TextStyle(
-                          fontFamily: 'AirbnbCereal',
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                left: 60,
-                top: 210,
-                child: Row(
-                  children: [
-                    Image.asset('assets/darkTick.png'),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      '7 jours de publicité',
-                      style: const TextStyle(
-                          fontFamily: 'AirbnbCereal',
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                left: 75,
-                top: 250,
-                child: Stack(
-                  children: [
-                    Row(
-                      children: [
-                        Image.asset('assets/Rectangle 4296.png'),
-                        SizedBox(
-                          width: 5,
-                        ),
-                      ],
-                    ),
-                    Positioned(
-                      top: 6,
-                      left: 23,
-                      child: Text(
-                        textAlign: TextAlign.center,
-                        'Acheter',
-                        style: TextStyle(
-                            fontFamily: 'AirbnbCereal',
-                            fontSize: 14,
-                            color: Colors.black,
-                            fontWeight: FontWeight.w400),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ]),
         ),
       ),
     );

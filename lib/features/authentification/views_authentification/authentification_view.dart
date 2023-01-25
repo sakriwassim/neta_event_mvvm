51f02cb1c599ce391e810afbf789f7851c2f2cbe
unknown <sakriwassim@gmail.com>
2023-01-25 23:30:56 +0100

    coming soon

--- lib/features/authentification/views_authentification/authentification_view.dart
+++ lib/features/authentification/views_authentification/authentification_view.dart
@@ -215,6 +215,46 @@ class AuthView extends StatelessWidget {
       );
     }
 
+    comingsoon() {
+      showGeneralDialog(
+        context: context,
+        barrierLabel: "Barrier",
+        barrierDismissible: true,
+        barrierColor: Colors.black.withOpacity(0.5),
+        transitionDuration: Duration(milliseconds: 700),
+        pageBuilder: (_, __, ___) {
+          return Center(
+            child: Container(
+              height: 240,
+              child: SizedBox.expand(
+                  child: Image.asset(
+                "assets/gif/coming_soon.gif",
+              )),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(40)),
+            ),
+          );
+        },
+        transitionBuilder: (_, anim, __, child) {
+          Tween<Offset> tween;
+          if (anim.status == AnimationStatus.reverse) {
+            tween = Tween(begin: Offset(-1, 0), end: Offset.zero);
+          } else {
+            tween = Tween(begin: Offset(1, 0), end: Offset.zero);
+          }
+
+          return SlideTransition(
+            position: tween.animate(anim),
+            child: FadeTransition(
+              opacity: anim,
+              child: child,
+            ),
+          );
+        },
+      );
+    }
+
     register() {
       if (formkey.currentState!.validate()) {
         Navigator.pushReplacement(
@@ -364,10 +404,8 @@ class AuthView extends StatelessWidget {
                   height: 10,
                 ),
                 InkWell(
-                  //InkWell
                   onTap: () {
-                    print("tapped");
-                    //  coming_soon.gif
+                    comingsoon();
                   },
                   child: CardGoogle(
                     image: imagegoogle,
@@ -381,17 +419,9 @@ class AuthView extends StatelessWidget {
                 ),
                 InkWell(
                   onTap: () {
-                    print("tapped");
+                    comingsoon();
                   },
-                  child:
-
-                      //  Container(
-                      //   color: Colors.blue,
-                      //   height: 50,
-                      //   width: 50,
-                      // ),
-
-                      CardGoogle(
+                  child: CardGoogle(
                     image: imagefacebook,
                     title: titleCF,
                     height: heightgoogle,
